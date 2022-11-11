9d4a75af70fad857a5504418f21853bb981bf949
= <=>
2022-11-11 20:29:00 -0300

    adicionar o exercicio sequencia de pares

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿int numeroatual = 0;
+int valordasoma = 0;
+Console.WriteLine("Digite o valor desejado...");
+int r = Convert.ToInt16(Console.ReadLine());
+while (numeroatual < r)
+{
+    valordasoma = valordasoma + numeroatual;
+    Console.WriteLine(numeroatual);
+    numeroatual = numeroatual +2;
+}
+Console.WriteLine(numeroatual);
+ 
